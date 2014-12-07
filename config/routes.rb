7981f3f94faf75e370ded55a3edb257b8b1e2249
jatin <jatin@browserstack.com>
2014-12-07 15:42:36 +0530

    sessions controller and login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
   get '/about' => 'static_pages#about'
   get 'signup' => 'users#new'
 
+  #sessions
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :microposts
 
   resources :users
