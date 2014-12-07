c81d82ea929c5e4056464200510bcf92ed3da9e2
jatin <jatin@browserstack.com>
2014-12-07 16:50:42 +0530

    email and password combo check.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,17 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      render plain: 'dude'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
+
 end
