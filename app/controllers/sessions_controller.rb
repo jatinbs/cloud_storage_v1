6a7e8cd960a56a8f8b1796843a6b73f0d8900e27
jatin <jatin@browserstack.com>
2014-12-08 18:27:24 +0530

    logout functionlaity added. added relation between file_uploads and user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_path
+      redirect_to user
     else
       render 'new'
     end
   end
 
   def destroy
-
+    logout if logged_in?
+    redirect_to root_url
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
+  has_many :file_uploads
 
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
