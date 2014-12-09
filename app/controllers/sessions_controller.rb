416363d9d252e0e5712e7206d59d35a128184d5f
jatin <jatin@browserstack.com>
2014-12-09 11:35:30 +0530

    lowercase email on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    email = params[:session][:email]
+    email.downcase! if email
+    user = User.find_by(email: email)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
