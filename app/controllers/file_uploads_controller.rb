b497815a25268ebc83d3acdc35f2a7159995e50d
jatin <jatin@browserstack.com>
2014-12-09 13:15:33 +0530

    file upload and save logic moved to model

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -23,19 +23,15 @@ class FileUploadsController < ApplicationController
   # POST /file_uploads.json
   def create
 
-    uuid = UUID.new
-    file_uuid = uuid.generate
-    if save_file(params[:upload_file], file_uuid)
-      @file_upload = current_user.file_uploads.build(file_upload_params(params[:upload_file], file_uuid))
+    @file_upload = current_user.file_uploads.new(file_upload_params)
 
-      respond_to do |format|
-        if @file_upload.save
-          format.html { redirect_to @file_upload, notice: 'File was successfully uploaded.' }
-          format.json { render :show, status: :created, location: @file_upload }
-        else
-          format.html { render :new }
-          format.json { render json: @file_upload.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @file_upload.save
+        format.html { redirect_to @file_upload, notice: 'File was successfully uploaded.' }
+        format.json { render :show, status: :created, location: @file_upload }
+      else
+        format.html { render :new }
+        format.json { render json: @file_upload.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,7 +39,6 @@ class FileUploadsController < ApplicationController
   # DELETE /file_uploads/1
   # DELETE /file_uploads/1.json
   def destroy
-
     @file_upload.destroy
     respond_to do |format|
       format.html { redirect_to file_uploads_url, notice: 'File upload was successfully destroyed.' }
@@ -54,26 +49,13 @@ class FileUploadsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
 
-    def save_file(og_file, filename)
-      directory = "uploads/"
-      path = File.join(directory, filename)
-      filesize = File.open(path, "wb") { |f| f.write(og_file.read)}
-    end
-
-
     def set_file_upload
       @file_upload = FileUpload.find(params[:id])
       current_user? @file_upload.user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def file_upload_params(og_file, filename)
-      file_upload_params = {
-        file_name: og_file.original_filename,
-        file_path: "uploads/#{filename}",
-        mime_type: og_file.content_type,
-        file_hash: filename
-      }
-      return file_upload_params
+    def file_upload_params
+      params.permit(:upload_file)
     end
 end
--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -1,6 +1,35 @@
 class FileUpload < ActiveRecord::Base
 
   belongs_to :user
-  validates :user_id, presence:true
+  attr_accessor :upload_file
+  # validates :user_id, presence:true
+  validates :upload_file, presence: true
+
+  before_validation :process_file
+  before_save :save_file
+
+  def process_file
+
+    # puts "file_size:" + File.size(self.upload_file.tempfile.path).to_s
+
+    uuid = UUID.new
+    file_hash = uuid.generate
+    puts self.upload_file
+
+    self.file_name = self.upload_file.original_filename
+    self.file_path = "uploads/#{file_hash}"
+    self.mime_type = self.upload_file.content_type
+    self.file_hash = file_hash
+  end
+
+  def save_file
+    directory = "uploads/"
+    path = File.join(directory, self.file_hash)
+    File.open(path, "wb") { |f| f.write(self.upload_file.read)}
+  end
+
+  def after_destroy
+    File.delete(self.file_path) if File.exist?(self.file_path)
+  end
 
 end
