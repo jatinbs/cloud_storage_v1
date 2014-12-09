c31dcabc2fb7a2cd707f88a61b6bb80d4c835e8f
jatin <jatin@browserstack.com>
2014-12-09 17:51:43 +0530

    signup tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
+  before do
+    @valid_attributes = {
+      name: 'test name',
+      email: 'test@example.com',
+      password: 'test123',
+      password_confirmation: 'test123'
+    }
+  end
+
+
   describe "GET new" do
     it "returns http success" do
       get :new
@@ -11,30 +21,30 @@ RSpec.describe UsersController, :type => :controller do
 
   describe "GET create" do
     it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+      post :create, {user: @valid_attributes}
+      expect(response).to redirect_to(login_path)
     end
   end
 
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET update" do
+  #   it "returns http success" do
+  #     get :update
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET destroy" do
+  #   it "returns http success" do
+  #     get :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
